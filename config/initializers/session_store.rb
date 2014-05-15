f4200aca2303a5516887ead99bd1bf06151f8d14
Special Leung <specialcyci@gmail.com>
2014-05-15 17:44:04 +0800

    Update Markdown style.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
 SpecialBlog::Application.config.session_store :cookie_store, key: '_SpecialBlog_session'
+SpecialBlog::Application.config.secret_key_base = "aasdqwe123123uocdkadlj123"
